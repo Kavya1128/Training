540c0c54456f4c0d9d740da116f93fe9f9b9c245
Kavya1128 <kavyakavya03861@gmail.com>
2025-01-21 11:48:39 +0530

    added 21-1-25

---
+++ Program.js
@@ -0,0 +1,16 @@
+let userDetails = {
+    name : "Kavya",
+    age : 25,
+    city : "Hyderabad"
+}
+console.log(userDetails);
+console.log(userDetails.name);
+console.log(typeof(userDetails));
+
+let x = JSON.stringify(userDetails);
+console.log(x);
+console.log(x.name);
+console.log(typeof(x));
+
+let y = JSON.parse(x);
+console.log(y);
---
+++ webstorage.js
@@ -0,0 +1,10 @@
+sessionStorage.setItem("user1" , "Kavya");
+sessionStorage.setItem("user2" , "Vamshi");
+sessionStorage.setItem("user3" , "Pavithra");
+sessionStorage.setItem("user4" , "Sanjay");
+sessionStorage.setItem("user5" , "Rishi");
+sessionStorage.setItem("user6" , "Sharvan");
+
+let user6  = sessionStorage.getItem("user6");
+console.log(user6);
+sessionStorage.removeItem("user1");
